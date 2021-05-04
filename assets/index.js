c5aa2c84edac08110ca5f7115e9d05bbaf4cb8c5
avtr-sergio10 <sergio.delarosa@avattar.com>
2021-05-04 15:16:12 -0500

    iframe update

--- assets/index.js
+++ assets/index.js
@@ -1,3 +1,3 @@
 console.log('hello world!');
 
-window.location.href ='https://appserver.dealpath.com/saml/sso/dealpath';
+// window.location.href ='https://appserver.dealpath.com/saml/sso/dealpath';
